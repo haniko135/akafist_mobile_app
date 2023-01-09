1da33c10f50b52d8c5a2a8a682768a2012e5bd81
Nastya Izotina <nastya.izotina@mail.ru>
2023-01-09 18:32:01 +0300

    fix bag with seekbar and playAndStop button

--- app/src/main/java/com/example/akafist/fragments/MolitvyOfflineFragment.java
+++ app/src/main/java/com/example/akafist/fragments/MolitvyOfflineFragment.java
@@ -5,6 +5,7 @@ import android.media.MediaPlayer;;
 import android.os.Bundle;
 
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -13,9 +14,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.example.akafist.R;
 import com.example.akafist.databinding.FragmentMolitvyOfflineBinding;
+
+import java.io.IOException;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -27,6 +31,7 @@ public class MolitvyOfflineFragment extends Fragment {
     private MediaPlayer mediaPlayer;
     private SeekBar seekBar;
     private ImageButton playStopButton;
+    private TextView checking, seekBarHint;
     FragmentMolitvyOfflineBinding molitvyOfflineBinding;
 
     public MolitvyOfflineFragment() {
@@ -34,8 +39,7 @@ public class MolitvyOfflineFragment extends Fragment {
     }
 
     public static MolitvyOfflineFragment newInstance() {
-        MolitvyOfflineFragment fragment = new MolitvyOfflineFragment();
-        return fragment;
+        return new MolitvyOfflineFragment();
     }
 
     @Override
@@ -90,7 +94,7 @@ public class MolitvyOfflineFragment extends Fragment {
         return view;
     }
 
-    public void playMolitva(int num){
+    public void playMolitva(int num) {
         switch (num){
             case 1:
                 play(R.raw.molitva_po_soglasheniyu);
@@ -107,6 +111,12 @@ public class MolitvyOfflineFragment extends Fragment {
     @SuppressLint("ClickableViewAccessibility")
     public void play(int uriRaw){
         mediaPlayer = MediaPlayer.create(getContext(), uriRaw);
+        mediaPlayer.setVolume(0.5f, 0.5f);
+        mediaPlayer.setLooping(false);
+
+        seekBarHint = getView().findViewById(R.id.seekBarHint);
+        playStopButton = getView().findViewById(R.id.imageButtonPlay);
+        checking = getView().findViewById(R.id.checking);
 
         seekBar = getView().findViewById(R.id.durationBarMolitvy);
         seekBar.setMax(mediaPlayer.getDuration());
@@ -120,16 +130,74 @@ public class MolitvyOfflineFragment extends Fragment {
                 return false;
             }
         });
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                checking.setText(String.valueOf(seekBar.getProgress()));
+                seekBarHint.setVisibility(View.VISIBLE);
+                int x = (int) Math.ceil(i / 1000f);
+
+                if (x < 10)
+                    seekBarHint.setText("0:0" + x);
+                else if(x > 10 && x < 60)
+                    seekBarHint.setText("0:" + x);
+                else {
+                    int min = x / 60, sec = x % 60;
+                    if (min < 10)
+                        if(sec < 10)
+                            seekBarHint.setText("0"+ min + ":0" + sec);
+                        else
+                            seekBarHint.setText("0"+ min + ":" + sec);
+                    else
+                        if(sec < 10)
+                            seekBarHint.setText(min + ":0" + sec);
+                        else
+                            seekBarHint.setText(min + ":" + sec);
+
+                }
+
+                double percent = i / (double) seekBar.getMax();
+                int offset = seekBar.getThumbOffset();
+                int seekWidth = seekBar.getWidth();
+                int val = (int) Math.round(percent * (seekWidth - 2 * offset));
+                int labelWidth = seekBarHint.getWidth();
+                seekBarHint.setX(offset + seekBar.getX() + val
+                        - Math.round(percent * offset)
+                        - Math.round(percent * labelWidth / 2));
+
+                if(i > 0 && !mediaPlayer.isPlaying()){
+                    mediaPlayer.seekTo(seekBar.getProgress());
+                }
+
+                /*if (i > 0 && mediaPlayer != null && !mediaPlayer.isPlaying()) {
+                    clearMediaPlayer();
+                    playStopButton.setImageResource(android.R.drawable.ic_media_play);
+                    seekBar.setProgress(0);
+                }*/
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                seekBarHint.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                seekBarHint.setVisibility(View.INVISIBLE);
+                if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+                    mediaPlayer.seekTo(seekBar.getProgress());
+                }
+            }
+        });
         playAndStop();
     }
 
     public void playAndStop(){
         if (!mediaPlayer.isPlaying()) {
-            playStopButton = getView().findViewById(R.id.imageButtonPlay);
             playStopButton.setImageResource(android.R.drawable.ic_media_pause);
             mediaPlayer.start();
         }else {
-            playStopButton = getView().findViewById(R.id.imageButtonPlay);
             playStopButton.setImageResource(android.R.drawable.ic_media_play);
             mediaPlayer.pause();
         }
@@ -139,9 +207,13 @@ public class MolitvyOfflineFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         if(mediaPlayer != null) {
-            mediaPlayer.stop();
-            mediaPlayer.release();
-            mediaPlayer = null;
+            clearMediaPlayer();
         }
+    }
+
+    private void clearMediaPlayer(){
+        mediaPlayer.stop();
+        mediaPlayer.release();
+        mediaPlayer = null;
     }
 }
