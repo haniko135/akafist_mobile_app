abb3048459142598868a75b548d47b2497d033ee
Nastya Izotina <nastya.izotina@mail.ru>
2023-01-14 19:08:48 +0300

    add recycleview to EverydayFragment

--- app/src/main/java/com/example/akafist/fragments/EverydayFragment.java
+++ app/src/main/java/com/example/akafist/fragments/EverydayFragment.java
@@ -1,17 +1,26 @@
 package com.example.akafist.fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.FragmentKt;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.akafist.R;
+import com.example.akafist.databinding.FragmentEverydayBinding;
+import com.example.akafist.models.PrayersModels;
+import com.example.akafist.recyclers.PrayersRecyclerAdapter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,13 +29,14 @@ import com.example.akafist.R;
  */
 public class EverydayFragment extends Fragment {
 
+    FragmentEverydayBinding everydayBinding;
+
     public EverydayFragment() {
         // Required empty public constructor
     }
 
     public static EverydayFragment newInstance() {
-        EverydayFragment fragment = new EverydayFragment();
-        return fragment;
+        return new EverydayFragment();
     }
 
     @Override
@@ -46,17 +56,84 @@ public class EverydayFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_everyday, container, false);
 
-        view.findViewById(R.id.textView7).setOnClickListener(new View.OnClickListener() {
+        everydayBinding = FragmentEverydayBinding.inflate(getLayoutInflater());
+
+        everydayBinding.everydayTime8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Bundle bundle = new Bundle();
-                bundle.putInt("prevMenu", R.id.action_prayerFragment_to_everydayFragment);
-                bundle.putInt("largeText", R.string.large_text_3);
-                FragmentKt.findNavController(getParentFragment()).navigate(R.id.action_everydayFragment_to_prayerFragment, bundle);
+                everydayBinding.everydayRv5.setVisibility(View.INVISIBLE);
+                everydayBinding.everydayRv6.setVisibility(View.INVISIBLE);
+                everydayBinding.everydayRv8.setVisibility(View.VISIBLE);
+                everydayBinding.everydayTime5.setBackgroundColor(Color.TRANSPARENT);
+                everydayBinding.everydayTime6.setBackgroundColor(Color.TRANSPARENT);
+                everydayBinding.everydayTime8.setBackgroundColor(getResources().getColor(R.color.beige_200));
             }
         });
-        return view;
+
+        everydayBinding.everydayTime5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                everydayBinding.everydayRv8.setVisibility(View.INVISIBLE);
+                everydayBinding.everydayRv6.setVisibility(View.INVISIBLE);
+                everydayBinding.everydayRv5.setVisibility(View.VISIBLE);
+                everydayBinding.everydayTime8.setBackgroundColor(Color.TRANSPARENT);
+                everydayBinding.everydayTime6.setBackgroundColor(Color.TRANSPARENT);
+                everydayBinding.everydayTime5.setBackgroundColor(getResources().getColor(R.color.beige_200));
+            }
+        });
+
+        everydayBinding.everydayTime6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                everydayBinding.everydayRv5.setVisibility(View.INVISIBLE);
+                everydayBinding.everydayRv8.setVisibility(View.INVISIBLE);
+                everydayBinding.everydayRv6.setVisibility(View.VISIBLE);
+                everydayBinding.everydayTime5.setBackgroundColor(Color.TRANSPARENT);
+                everydayBinding.everydayTime8.setBackgroundColor(Color.TRANSPARENT);
+                everydayBinding.everydayTime6.setBackgroundColor(getResources().getColor(R.color.beige_200));
+            }
+        });
+
+        List<PrayersModels> prayersModels1 = prayersNameList(1);
+        everydayBinding.everydayRv8.setLayoutManager(new LinearLayoutManager(getContext()));
+        everydayBinding.everydayRv8.setAdapter(new PrayersRecyclerAdapter(prayersModels1, this));
+
+        List<PrayersModels> prayersModels2 = prayersNameList(2);
+        everydayBinding.everydayRv5.setLayoutManager(new LinearLayoutManager(getContext()));
+        everydayBinding.everydayRv5.setAdapter(new PrayersRecyclerAdapter(prayersModels2, this));
+
+        List<PrayersModels> prayersModels3 = prayersNameList(3);
+        everydayBinding.everydayRv6.setLayoutManager(new LinearLayoutManager(getContext()));
+        everydayBinding.everydayRv6.setAdapter(new PrayersRecyclerAdapter(prayersModels3, this));
+
+        return everydayBinding.getRoot();
+    }
+
+    private List<PrayersModels> prayersNameList(int n){
+        List<PrayersModels> prayersModels = new ArrayList<>();
+        List<Integer> prayersLinks = new ArrayList<>();
+        switch (n){
+            case 1:
+                someFunc(prayersModels, prayersLinks, R.array.everyday_names_8, R.string.large_text_5);
+                break;
+            case 2:
+                someFunc(prayersModels, prayersLinks, R.array.everyday_names_5, R.string.large_text_6);
+                break;
+            case 3:
+                someFunc(prayersModels,prayersLinks, R.array.everyday_names_6, R.string.large_text_6);
+                break;
+        }
+        return prayersModels;
+    }
+
+    private void someFunc(List<PrayersModels> prayersModels, List<Integer> prayersLinks, int array, int string){
+        List<String> prayersNames = Arrays.asList(getResources().getStringArray(array));
+        for(int i = 0; i < prayersNames.size(); i++){
+            prayersLinks.add(string);
+        }
+        for (int i = 0; i < prayersNames.size(); i++){
+            prayersModels.add(new PrayersModels(prayersNames.get(i), prayersLinks.get(i)));
+        }
     }
 }
--- app/src/main/java/com/example/akafist/recyclers/PrayersRecyclerAdapter.java
+++ app/src/main/java/com/example/akafist/recyclers/PrayersRecyclerAdapter.java
@@ -2,16 +2,19 @@ package com.example.akafist.recyclers;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.FragmentKt;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.akafist.R;
+import com.example.akafist.fragments.EverydayFragment;
 import com.example.akafist.fragments.PsaltirFragment;
 import com.example.akafist.models.PrayersModels;
 
@@ -20,9 +23,14 @@ import java.util.List;
 public class PrayersRecyclerAdapter extends RecyclerView.Adapter<PrayersRecyclerAdapter.PrayersViewHolder> {
 
     private List<PrayersModels> prayers;
-    private PsaltirFragment fragment;
+    private Fragment fragment;
 
     public PrayersRecyclerAdapter(List<PrayersModels> prayers, PsaltirFragment fragment) {
+        this.prayers = prayers;
+        this.fragment = fragment;
+    }
+
+    public PrayersRecyclerAdapter(List<PrayersModels> prayers, EverydayFragment fragment) {
         this.prayers = prayers;
         this.fragment = fragment;
     }
@@ -35,7 +43,6 @@ public class PrayersRecyclerAdapter extends RecyclerView.Adapter<PrayersRecycler
     }
 
 
-    // ошибка в передаче фрагмента сюда
     @Override
     public void onBindViewHolder(@NonNull PrayersViewHolder holder, @SuppressLint("RecyclerView") int position) {
         holder.prayersListItem.setText(prayers.get(position).getNamePrayer());
@@ -43,9 +50,13 @@ public class PrayersRecyclerAdapter extends RecyclerView.Adapter<PrayersRecycler
             @Override
             public void onClick(View view) {
                 Bundle bundle = new Bundle();
-                bundle.putInt("prevMenu",R.id.action_prayerFragment_to_psaltirFragment);
+                if(fragment.getClass() == EverydayFragment.class) {
+                    bundle.putInt("prevMenu", R.id.action_prayerFragment_to_everydayFragment);
+                }else if(fragment.getClass() == PsaltirFragment.class){
+                    bundle.putInt("prevMenu", R.id.action_prayerFragment_to_psaltirFragment);
+                }
                 bundle.putInt("largeText", prayers.get(position).getTextPrayer());
-                FragmentKt.findNavController(fragment).navigate(R.id.action_psaltirFragment_to_prayerFragment,bundle);
+                FragmentKt.findNavController(fragment).navigate(R.id.prayerFragment,bundle);
             }
         });
     }
